004ac2a780ccfd757862ae79ca41f16633dcd773
Iñaki Lozano <kakitolozano@gmail.com>
2020-11-14 16:34:51 -0300

    i

--- src/tipos/modelos/GestorTipos.java
+++ src/tipos/modelos/GestorTipos.java
@@ -2,8 +2,24 @@
 package tipos.modelos;
 
 import interfaces.IGestorTipos;
+import java.util.ArrayList;
 
 
 public class GestorTipos implements IGestorTipos{
+
+    @Override
+    public String nuevoTipo(String nombre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Tipo> verTipos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Tipo verTipo(String nombre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
